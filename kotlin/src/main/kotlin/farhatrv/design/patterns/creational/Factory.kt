9631517330104fdc484d343538ed4f161fe84444
farhatrv <farhat.abbas@skyschedule.com>
2023-02-22 18:51:52 +0530

    [Kotlin] add enum based Factory design pattern

--- kotlin/src/main/kotlin/farhatrv/design/patterns/creational/Factory.kt
+++ kotlin/src/main/kotlin/farhatrv/design/patterns/creational/Factory.kt
@@ -22,16 +22,49 @@ abstract class Computer {
     }
 }
 
+enum class CommonComputerTypes {
+    SERVER,
+    PC
+}
+
+enum class SuperComputerTypes {
+    MASTER_BASED_CLUSTER,
+    MULTI_NODE_CLUSTER
+}
+
 class PC(override val rAM: String, override val hDD: String, override val cPU: String) : Computer()
 
 class Server(override val rAM: String, override val hDD: String, override val cPU: String) : Computer()
 
+class MultiNodeSuperComputer(
+    override val rAM: String, override val hDD: String, override val cPU: String, private val numberOfNodes: Int
+) : Computer() {
+    override fun toString(): String {
+        return super.toString() + ", number of nodes=" + numberOfNodes
+    }
+}
+
 object ComputerFactory {
     // child object creation method should either be static or should be served via a singleton object
-    fun getComputer(type: String, ram: String, hdd: String, cpu: String): Computer? {
-        return when (type) {
-            "PC" -> PC(ram, hdd, cpu)
-            "Server" -> Server(ram, hdd, cpu)
+    fun getComputer(type: String, ram: String, hdd: String, cpu: String): Computer {
+        return when (enumValueOf<CommonComputerTypes>(type)) {
+            CommonComputerTypes.PC -> PC(ram, hdd, cpu)
+            CommonComputerTypes.SERVER -> Server(ram, hdd, cpu)
+        }
+    }
+}
+
+class ComputerFactoryProvider {
+    inline fun <reified M : Enum<M>> createComputer(
+        type: String,
+        ram: String, hdd: String,
+        cpu: String,
+        numberOfNodes: Int = 2
+    ): Computer? {
+        return when (enumValueOf<M>(type)) {
+            CommonComputerTypes.SERVER -> Server(ram, hdd, cpu)
+            CommonComputerTypes.PC -> PC(ram, hdd, cpu)
+            SuperComputerTypes.MULTI_NODE_CLUSTER -> MultiNodeSuperComputer(ram, hdd, cpu, numberOfNodes)
             else -> null
         }
     }
@@ -41,9 +74,18 @@ object Factory {
     @JvmStatic
     fun main(args: Array<String>) {
         val logger: Logger = LoggerFactory.getLogger(Factory::class.java)
-        val pc = ComputerFactory.getComputer("Server", "2 GB", "50 GB", "2.4 GHz")
+
+        // factory 1 - type defined on 1 level
+        val pc = ComputerFactory.getComputer("SERVER", "2 GB", "50 GB", "2.4 GHz")
         val server = ComputerFactory.getComputer("PC", "16 GB", "1 TB", "2.9 GHz")
         logger.info(pc.toString())
         logger.info(server.toString())
+
+        // factory 2 - FactoryProvider - type defined on 2 levels
+        val computerFactoryProvider = ComputerFactoryProvider()
+        val pc1 = computerFactoryProvider.createComputer<SuperComputerTypes>(
+            "MULTI_NODE_CLUSTER", "16 GB", "1 TB", "2.9 GHz", 16
+        )
+        logger.info(pc1.toString())
     }
 }
